e8648092d26d337725b3cbfc771e003c687291e4
root <root@railNginx12.10>
2014-04-17 07:11:40 +0000

    first Batch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App001::Application.config.secret_key_base = '430d038358acdd71305b3d6c637ec8189047e257200b0ae52745d644f48734397f6af1226b3fa563b28f521901dcc31c2d0fc704507db7a66d0335b6a38b32e2'
